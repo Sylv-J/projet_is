861afb8c1ed68939a058e20d034f74e2f314211e
Xousi <alexispetitjean@hotmail.fr>
2016-01-02 22:36:04 +0100

    CORRECT reimplantation du fichier master_db.php etrangement disparu

---
+++ master_db.php
@@ -0,0 +1,25 @@
+<?php
+class masterDB{
+
+	private static $instance = null;
+	private $db;
+
+	public static function getDB(){
+		// avoid double instantiation
+		if(is_null(static::$instance)){
+			static::$instance = new masterDB();
+		}
+		return static::$instance->db;
+	}
+
+	private function __construct(){
+		try{
+			$this->db = new PDO("mysql:host=localhost","root","",array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+			$this->db->query("CREATE DATABASE IF NOT EXISTS projetis");
+			$this->db->query("use projetis");
+		}
+		catch(Exception $e){
+			die("Fatal error while loading database : ".$e->getMessage());
+		}
+	}
+}
