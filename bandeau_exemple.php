07c37b0e6617aee6c2c63bf3d5e46bbeca839536
Sylv-J <sylvain.jonchery@depinfonancy.net>
2016-01-02 16:41:17 +0100

    Découpe du fichier index en plusieurs fichier PHP Cela permet d'être plus lisible et permettra de mettre des conditions d'affichage en fonction de l'utilisateur.

---
+++ bandeau_exemple.php
@@ -0,0 +1,44 @@
+<div class="container">
+    <!-- Example row of columns -->
+    <div class="row">
+        <div class="col-md-4">
+            <h2>Progression</h2>
+            <h3>Exercices corrigés</h3>
+            <div class="progress">
+                <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%;">
+                    60%
+                </div>
+            </div>
+            <h3>Élèves évalués</h3>
+            <div class="progress">
+                <div class="progress-bar" role="progressbar" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100" style="width: 20%;">
+                    20%
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <h2>Informations</h2>
+            <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+            <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+        </div>
+        <div class="col-md-4">
+            <h2>Dates clés</h2>
+            <div class="list-group">
+                <a href="#" class="list-group-item active">
+                    <h4 class="list-group-item-heading">Mise en ligne des copies</h4>
+                    <p class="list-group-item-text">O6/06/2017</p>
+                </a>
+                <a href="#" class="list-group-item">
+                    <h4 class="list-group-item-heading">Achèvement des corrections</h4>
+                    <p class="list-group-item-text">O6/06/2016</p>
+                </a>
+            </div>
+        </div>
+    </div>
+
+    <hr>
+
+    <footer>
+        <p>&copy; 2015 Mines Nancy</p>
+    </footer>
+</div> <!-- /container -->
