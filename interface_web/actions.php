7c8cc9c3a3b5d4fcd19e9dec4007504bd878bfc8
ajubin <antoine.jubin@depinfonancy.net>
2016-01-03 19:59:52 +0100

    Ajout des boutons propre a chaque user, role obsolete

--- interface_web/actions.php
+++ interface_web/actions.php
@@ -6,8 +6,8 @@ choisir un nombre entre 1 et 4 pour chnager l'affichage de la page
 2 : Secretaire
 3 : Correcteur
 4 : Chairman
-*/
-$test=3;
+
+$test=1;
 switch ($test) {
     case 1:
         include("roles/administrateur.php");
@@ -22,4 +22,31 @@ switch ($test) {
 		include("roles/chairman.php");
 		break;
 }
+*/
+?>
+<?php 
+	$rights=array(
+		"administrateur" => array("Creer compte","associer des notes","réinitialiser mdp"),
+		"secretaire" => array("scanner des copies","gestion des erreurs","mail de rappel","inviter des correcteurs"),
+		"correcteur" => array("Corriger","Revoir","Demander", "Statistiques")
+	);
+	
+	$user="administrateur";
+	
+	//Mise en place du style 
+	echo"
+	<div class='col-md-4'>
+		<h2>Actions de $user</h2>
+		<div class='list-group'>
+	";
+	//Les boutons utilisables
+	foreach($rights[$user] as $actions){
+		echo "
+		<a href='#' class='list-group-item'>$actions</a>
+		";
+	}
+	//fermeture des balises
+	echo"
+	</div></div>
+	";
 ?>
