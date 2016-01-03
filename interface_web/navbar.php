77a0e212943f048822796dd01686268ceda5a3fe
Sylv-J <sylvain.jonchery@depinfonancy.net>
2016-01-03 01:21:50 +0100

    ajout du bouton page perso dans le menu

--- interface_web/navbar.php
+++ interface_web/navbar.php
@@ -33,7 +33,7 @@
                 <li class="dropdown">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
                     <ul class="dropdown-menu">
-                        <li><a href="#">Action</a></li>
+                        <li><a href="perso.php">Page perso</a></li>
                         <li role="separator" class="divider"></li>
                         <li><a href="../users/logout.php">déconnexion</a></li>
                     </ul>
