8db41b795f5590ca739051a91257f7e4cdc6eaed
ajubin <antoine.jubin@depinfonancy.net>
2016-01-04 22:41:21 +0100

    ajout des boutons d'actions pour chaque utilisateur, tester seulement avec la secrÃ©taire pour le moment user_tasks.php devient inutile

--- interface_web/actions.php
+++ interface_web/actions.php
@@ -5,21 +5,29 @@
 get actions
 pour chaque action, on fait un bouton
 */
-	
+$user = $_SESSION["group"];
+
+$rights = array(
+'administrateur' => array('Modifier les droits'),
+'secretaire' => array('Scanner'),
+'correcteur' => array('Corriger')
+);	
 	//Mise en place du style 
 	echo"
 	<div class='col-md-4'>
 		<h2>Actions de $user</h2>
+		<form action='index.php' method='get'>
 		<div class='list-group'>
 	";
 	//Les boutons utilisables
 	foreach($rights[$user] as $actions){
 		echo "
-		<a href='#' class='list-group-item'>$actions</a>
+		<button type='submit' name='page_to_load' value='$actions' class='btn'>$actions</button>
 		";
 	}
 	//fermeture des balises
 	echo"
-	</div></div>
+	
+	</div></form></div>
 	";
 ?>
