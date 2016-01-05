6e77c669b02f94eb2099a9b835570023bf9527f4
Xousi <alexispetitjean@hotmail.fr>
2016-01-05 01:02:08 +0100

    ADD taskDirectories rassembler les noms de chaque pages gerant une tache particuliere, UPD index en consequence

---
+++ interface_web/tasksDirectories.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+liste des tâches possibles, avec adresse de redirection associés
+*******************TO DO*******************
+une fois les pages crées, remplacer convenablement les noms de pages dans la
+destination
+*******************************************
+*/
+$tasksDirectories = array(
+  'Modifier les droits' => '__PageDeDestination1__',
+  'Scanner' => 'bandeau_scan.php',
+  'Corriger' => '__PageDeDestination3__'
+  );
+
+?>
