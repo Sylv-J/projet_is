d9bd51a42d490e9dcbceac8e89609521efa1bf88
loisfa <lois94@hotmail.fr>
2016-01-11 19:49:32 +0100

    Fichier test permettant de vérifier la cohérence entre tâches autorisées et type d'utilisateur.
    	nouveau fichier: [User_tests]test_user_rights.php

---
+++ interface_web/[User_tests]test_user_rights.php
@@ -0,0 +1,61 @@
+<?php
+
+	require_once "actions.php";
+	require_once "tasksDirectories.php";
+
+	// déclaration des rôles
+	$administrateur = 'administrateur';
+	$secretaire = 'secretaire';
+	$correcteur = 'correcteur';
+
+	// déclaration des tâches
+	$modifier = 'Modifier les droits';
+	$scanner = 'Scanner';
+  $corriger = 'Corriger';
+	//$revoir = 'Revoir une copie'; droit récemment temporairement révoqué
+	$register = 'register';
+	$logout = 'logout';
+
+	// modifier les pages le cas échéant
+	$pageDestModifier = '__PageDeDestination1__';
+	$pageDestScanner = 'ajoutercopie.php';
+	$pageDestCorriger = 'bandeau_correction.php';
+	$pageDestRevoir = '__PageDeDestination__';
+	$pageDestRegister = '../users/registration.php';
+  $pageDestLogout = '../users/logout.php';
+
+
+	/*
+	* VERIFICATION DE L'ATTRIBUTION DES DROITS
+	*/
+
+	// vérification des droits pour l'administrateur
+	assert(in_array($modifier, $rights[$administrateur]));
+	assert(!in_array($scanner, $rights[$administrateur]));
+	assert(!in_array($corriger, $rights[$administrateur]));
+	//assert(!in_array($revoir, $rights[$administrateur]));
+
+	// vérification des droits pour le secrétaire
+	assert(!in_array($modifier, $rights[$secretaire]));
+	assert(in_array($scanner, $rights[$secretaire]));
+	assert(!in_array($corriger, $rights[$secretaire]));
+	//assert(!in_array($revoir, $rights[$secretaire]));
+
+	// vérification des droits pour le correcteur
+	assert(!in_array($modifier, $rights[$correcteur]));
+	assert(!in_array($scanner, $rights[$correcteur]));
+	assert(in_array($corriger, $rights[$correcteur]));
+	// assert(in_array($revoir, $rights[$correcteur])); droit temporairement révoqué
+
+	/*
+	* VERIFICATION DES PAGES DE DESTINATION
+	*/
+	assert($pageDestModifier == $tasksDirectories[$modifier]);
+	assert($pageDestScanner == $tasksDirectories[$scanner]);
+	assert($pageDestCorriger == $tasksDirectories[$corriger]);
+	//assert($pageDestRevoir == $tasksDirectories[$revoir]); page temporairement inexistante
+	assert($pageDestRegister == $tasksDirectories[$register]);
+	assert($pageDestLogout == $tasksDirectories[$logout]);
+
+	echo "Exécution du script <strong>".$_SERVER['PHP_SELF']."</strong> terminée - <strong>0 erreur</strong>.";
+?>
