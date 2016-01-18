5c0634bdcc59a983c2f86a5dd3c390bd6a2cfeb7
ACardonne <alban.cardonne@depinfonancy.net>
2016-01-18 19:28:30 +0100

    formulaire ajout table concours
    formulaire_ajout_table.php permet de créer 2 nouvelles tables dans la
    base de données de type nom_concoursusers et nom_concoursunits en
    appelant l'action action_ajout_table.php

---
+++ utils/action_ajout_table.php
@@ -0,0 +1,15 @@
+<?php
+include_once("../master_db.php");
+$db = masterDB::getDB();
+$tableFile = fopen("tables_struct","r");
+$nomtableusers = $_POST['NomConcours']."users";
+$nomtableunits = $_POST['NomConcours']."units";
+
+while($line = fgets($tableFile)){
+	$db->query("CREATE TABLE IF NOT EXISTS $nomtableusers LIKE users");  // si mon_nom_de_tableuser existe pas je le créé avec les même champs que ceux de la table users
+	$db->query("CREATE TABLE IF NOT EXISTS $nomtableunits LIKE units");  // si mon_nom_de_tableunits existe pas je le créé ...
+
+}
+
+
+?>
