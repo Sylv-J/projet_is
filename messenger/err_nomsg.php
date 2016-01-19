e29f5df53c04919ba6119d2d4b3ccafd1f207f70
Bl4ckHole <corentinja@gmail.com>
2016-01-19 09:50:57 +0100

    Finalisation du système de messagerie
    Ce commit finalise le coeur du système de messagerie interne, qui est
    maintenant fonctionnel
    //TODO une vraie interface

---
+++ messenger/err_nomsg.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="refresh" content="3;URL=inbox.php">
+  <title>Erreur</title>
+</head>
+<body>
+  <center>
+    Erreur : le contenu demandé n'existe pas ou vous n'avez pas la permission d'y accéder, vous allez être redirigé automatiquement.
+  </center>
+</body>
--- messenger/inbox.php
+++ messenger/inbox.php
@@ -6,8 +6,8 @@ if(!isset($_SESSION["id"])){
 $box = isset($_POST["box"]) ? $_POST["box"] : "inbox";
 $names = array("inbox" => "Boîte de réception", "sendbox" => "Boîte d'envoi");
 $requests = array(
-  "inbox" => "SELECT l.mfrom c1, m.object obj, DATE_FORMAT(m.date,'%d/%m/%Y %H:%i') dated FROM msg_link l JOIN msg m ON m.id = l.id_msg WHERE l.mto = ?",
-  "sendbox" => "SELECT DISTINCT l.dest c1, m.object obj, DATE_FORMAT(m.date,'%d/%m/%Y %H:%i') dated FROM msg_link l JOIN msg m ON m.id = l.id_msg WHERE l.mfrom = ?"
+  "inbox" => "SELECT m.id mid, l.id lid, l.mfrom c1, m.object obj, DATE_FORMAT(m.date,'%d/%m/%Y %H:%i') dated FROM msg_link l JOIN msg m ON m.id = l.id_msg WHERE l.mto = ? ORDER BY dated DESC",
+  "sendbox" => "SELECT DISTINCT m.id id, l.id lid, l.dest c1, m.object obj, DATE_FORMAT(m.date,'%d/%m/%Y %H:%i') dated FROM msg_link l JOIN msg m ON m.id = l.id_msg WHERE l.mfrom = ? ORDER BY dated DESC"
 );
 
 //SQl requests
@@ -30,7 +30,7 @@ $req->execute(array($_SESSION["username"]));
     </tr>
     <?php if($data = $req->fetch()){
       do{?>
-      <tr>
+      <tr onclick="location.href='msg_read.php?<?php echo("mid=".$data["mid"]."&lid=".$data["lid"]); ?>'">
         <td><?php echo $data["c1"];  ?></td>
         <td><?php  echo $data["obj"]; ?></td>
         <td><?php  echo $data["dated"]; ?></td>
