803c77edd002e3c0c92c62fd226b041ab3dc5adc
ajubin <antoine.jubin@depinfonancy.net>
2016-01-24 17:04:43 +0100

    modifictions des chemins dans tasks directories

--- actions/tasksDirectories.php
+++ actions/tasksDirectories.php
@@ -12,7 +12,7 @@ $tasksDirectories = array(
   'Corriger' => 'correcteur/bandeau_correction.php',
   'Voir mes Stats' => 'correcteur/bandeau_stats_correcteur.php',
   'Statistiques' => 'chairman/statschairman.php',
-  'Stats Correcteur' => 'correcteur/bandeau_stats_chairman.php',
+  'Stats Correcteur' => 'chairman/bandeau_stats_chairman.php',
   'Assignation des copies' => 'chairman/assignationcopies.php',
   'Ajouter concours' => 'administrateur/ajout_concours.php',
 
