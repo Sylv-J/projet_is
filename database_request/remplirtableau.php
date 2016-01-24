1d3c0d1bddbfb5f64e651016a4a05a2384cf2900
ajubin <antoine.jubin@depinfonancy.net>
2016-01-24 16:52:06 +0100

    Correction de bugs dans le nom des appels sql pour les stats du chairman

--- database_request/remplirtableau.php
+++ database_request/remplirtableau.php
@@ -1,14 +1,14 @@
 <?php
   include_once("../master_db.php");
   $db = masterDB::getDB();
-  $req = $db->query("SELECT id, username, mail, nbcopiesrestantes FROM users WHERE user_group = 'correcteur' ");
+  $req = $db->query("SELECT id, username, mail, units_remaining FROM users WHERE user_group = 'correcteur' ");
   while($donnees = $req->fetch()) {
        $id = $donnees["id"];
        $mail = $donnees["mail"];
        $username = $donnees["username"];
-       $nbcopiesrestantes =$donnees["nbcopiesrestantes"];
+       $nbcopiesrestantes =$donnees["units_remaining"];
        
-       echo "<tr> <td>".$username."</td> <td>".$id."</td> <td>".$nbcopiesrestantes."</td> <td>".$mail."</td></tr>";
+       echo "<tr> <td>'$username'</td> <td>'$id'</td> <td>'$nbcopiesrestantes'</td> <td>'$mail'</td></tr>";
         
   }
 
