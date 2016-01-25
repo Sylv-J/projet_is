3eb85d90f88e48ce0b12c2fd360b7797fad473e6
ajubin <antoine.jubin@depinfonancy.net>
2016-01-25 15:21:52 +0100

    Mise en page du tableau dans assignation copies

--- actions/chairman/assignationcopies.php
+++ actions/chairman/assignationcopies.php
@@ -1,21 +1,45 @@
 <div class ="jumbotron">
     <div class = "container">
       <h2> Assignation Copies </h2>
-      <form method="post" action="../interface_web/index.php">
-        <select name="unitsType">
-            <?php
-              include("../../paquets_copies/paquets_copies.php");
-              $list = array("Maths1","Physique");
-              //$list = getSubjects();
-              for ($i=0;$i<=sizeof($list);$i++) {
-                $value = $list[$i];
-                echo "<option value=$value>$value</option>" ;
-              }
-            ?>
 
-        </select>
-        <input type="submit" value="Assigner" title="Valider pour assigner" />
-      </form>
+      <!--Assignation des correcteurs sur une épreuve -->
+      <div class="col-md-3">
+        <form method="post" action="../interface_web/index.php">
+          <select name="unitsType">
+              <?php
+                include("../../paquets_copies/paquets_copies.php");
+                $list = array("Maths1","Physique");
+                //$list = getSubjects();
+                for ($i=0;$i<=sizeof($list);$i++) {
+                  $value = $list[$i];
+                  echo "<option value=$value>$value</option>" ;
+                }
+              ?>
+
+          </select>
+          <input type="hidden" name="page_to_load" value="Assignation des copies"/>
+          <input type="submit" value="Assigner" title="Valider pour assigner" />
+        </form>
+      </div>
+      
+      <!--Tableau affichant les udc assigné aux correcteurs-->
+      <div class="col-md-6">
+        <table class="table">
+          <thead>
+            <tr>
+              <th>Correcteur</th>
+              <th>Nombre de copies assignées</th>
+              <th>Matière</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <?php ?>
+          </tbody>
+
+        </table>
+
+      </div>
     </div>
 </div>
 
