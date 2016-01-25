24d79a830709d99fb2419c2cdcb9c2d3774ddf02
ACardonne <alban.cardonne@depinfonancy.net>
2016-01-25 15:41:03 +0100

    MaJ chairman création concours
    L'élève 0 est maintenant créé à l'ajout du concours par le chairman. Son
    id est 0 et idfather -1

--- database_request/action_ajout_table.php
+++ database_request/action_ajout_table.php
@@ -10,10 +10,10 @@ $nomtableunits = $_POST['NomConcours']."units";
 while($line = fgets($tableFile)){
 	$db->query("CREATE TABLE IF NOT EXISTS $nomtableusers LIKE users");  // si mon_nom_de_tableuser existe pas je le créé avec les même champs que ceux de la table users
 	$db->query("CREATE TABLE IF NOT EXISTS $nomtableunits LIKE units");  // si mon_nom_de_tableunits existe pas je le créé ...
-
+	
 }
 mkdir("../images/".$_POST['NomConcours']);
-
+$db->query("INSERT INTO $nomtableunits (id, id_father) VALUES ('0','-1')");
 header('Location: ../interface_web/index.php')
 
 ?>
