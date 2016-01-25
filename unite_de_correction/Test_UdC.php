78b980e80462be4be7752b86d21a520322bcbf4b
Playeurmania <alexandre.zirmi@depinfonancy.net>
2016-01-25 11:33:26 +0100

    Premiers tests

--- unite_de_correction/Test_UdC.php
+++ unite_de_correction/Test_UdC.php
@@ -4,6 +4,9 @@ include_once("UniteDeCorrection.php");
 //Les différents bloc sont en commentaires pour pouvoir tester la fonctionnalité correspondante. Enlever les commentaires pour tester.
 
 /*
+
+//bloc fromData
+ 
 $arrayData = array('id_father'=>'lolfather',
 		'id'=>'lolme',
 		'id_sons'=>'lol_son1,lol_son2,lol_son3',
@@ -25,7 +28,7 @@ $udc = UniteDeCorrection::getUnitById($udc->getId());
 
 echo $udc->getId();
 
-
+//bloc generateBareme, fromID et getUnitByID
 try
 {
 UniteDeCorrection::generateBareme("Maths1_Partie1_Exercice1_petita_3
