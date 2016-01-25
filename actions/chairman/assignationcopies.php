2c4d0cc725c85730e05186e7860dcaca6f93d479
ajubin <antoine.jubin@depinfonancy.net>
2016-01-25 14:41:52 +0100

    Assignation des copies, ajout du menu dÃ©roulant

--- actions/chairman/assignationcopies.php
+++ actions/chairman/assignationcopies.php
@@ -2,25 +2,20 @@
     <div class = "container">
       <h2> Assignation Copies </h2>
       <form method="post" action="../interface_web/index.php">
-      <p>
-      <select name="unitsType">
-          <?php
-            include(../paquets_copies/paquets_copies.php)
-            $list = getUnits();
-            for (i=0;sizeof($list);i++) {
-              $value = $list[i];
-              echo "<option value=$value>$value</option>" ;
-            }
-            
-          ?>
-         
-      </select>
+        <select name="unitsType">
+            <?php
+              include("../../paquets_copies/paquets_copies.php");
+              $list = array("Maths1","Physique");
+              //$list = getSubjects();
+              for ($i=0;$i<=sizeof($list);$i++) {
+                $value = $list[$i];
+                echo "<option value=$value>$value</option>" ;
+              }
+            ?>
 
-      <input type="submit" value="Assigner" title="Valider pour assigner" />
-
-</p>
-</form>
-        </form>
+        </select>
+        <input type="submit" value="Assigner" title="Valider pour assigner" />
+      </form>
     </div>
 </div>
 
