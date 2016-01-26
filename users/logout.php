a75c3e90ee0c86e0b4af694ab38291d8b5ee97cf
Sahli Ayoub <ayoub.sahli@depinfonancy.net>
2016-01-26 17:59:18 +0100

    Déconnexion utilisateur
    Fixation du bug de déconnexion
    Mise en page de la page de déconnexion

--- users/logout.php
+++ users/logout.php
@@ -1,12 +1,22 @@
+
 <?php
 if(!isset($_SESSION))
 {
   session_start();
 }
+header("refresh:3;URL=../interface_web/index.php");
 $_SESSION = array();
 session_destroy();
 //erase cookies
 setcookie("username","");
 setcookie("pwd","");
-echo "Vous avez été déconnecté";
 ?>
+<div class="jumbotron">
+  <div class="container">
+    <h2>
+    <center>
+      Déconnexion réussie.<br>Vous allez être redirigé vers la <a href="../interface_web/index.php">page d'acceuil</a> automatiquement.
+    </center>
+  </h2>
+  </div>
+</div>
