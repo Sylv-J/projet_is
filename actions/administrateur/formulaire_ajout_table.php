c6b5fd7c6ea1303dfaf43b32c30344573ef9b01c
Sahli Ayoub <ayoub.sahli@depinfonancy.net>
2016-01-26 09:16:32 +0100

    Correction Bug
    Correction bug d'include

--- actions/administrateur/formulaire_ajout_table.php
+++ actions/administrateur/formulaire_ajout_table.php
@@ -1,10 +1,10 @@
 <?php
-include_once("../../master_db.php");
+include_once("../master_db.php");
 $db = masterDB::getDB();
 //Pas de Check ici. On demande de rentrer un nom de concours, le check d'existence se fait dans action_ajout_table.php
 ?>
 <!-- HMTL code for the registration form -->
-<form action="../../database_request/action_ajout_table.php" method="post">
+<form action="../database_request/action_ajout_table.php" method="post">
 <h3>Inscription : </h3>
 <p>
 	Nom concours : <input type="text" name="NomConcours" required><br><br>
--- actions/chairman/action_ajout_eleve.php
+++ actions/chairman/action_ajout_eleve.php
@@ -1,5 +1,5 @@
 <?php
-include_once("../../master_db.php");
+include_once("../master_db.php");
 $db = masterDB::getDB();
 
 
@@ -17,32 +17,32 @@ $tab_ligne = array($_POST["id_eleve"]);
 for($i =0 ; $i<count($tab) ; $i++){         //on parcourt notre tableau contenant chaque lignes
 	$flag = 0;
 	$count = 0;
-	
+
 	$ligne = $tab[$i];
 	$ligne = trim($ligne);
 	$path = "../../$nomconcours/";
 	for($k = 0; $k<strlen($tab[$i]) ; $k++){  // on parcourt chaque caractère de notre ligne
-		if($tab[$i][$k] == "@"){					
+		if($tab[$i][$k] == "@"){
 			$flag = 1;
 			$ligne = rtrim ($ligne, "@");		//on supprime les @
-	
-			
+
+
 		}
 		if($tab[$i][$k] == "*"){
 			$count++;
 			$ligne = ltrim ($ligne, "*");		//on supprime les *
-			
+
 		}
 	}										// maintenant on sait comment est notre ligne
 	for($j = (count($tab_ligne)-1); $j>$count;$j--){
 				//echo $tab_ligne[$j];
 				unset($tab_ligne[$j]);
 	}
-	
+
 	$tab_ligne[$count+1] = $ligne;
-	
+
 	$id[$count+1] = "$id[$count]"."$ligne";
-	
+
 
 	$idmoi = "$id[$count]"."$ligne";
 
@@ -50,11 +50,11 @@ for($i =0 ; $i<count($tab) ; $i++){         //on parcourt notre tableau contenan
 	//$db->query("INSERT INTO minesunits (id, id_father) VALUES ('$idmoi','$id[$count]')");
 	if($flag == 1){					//l'@ indique lorsque on est en dossier sans sous dossier
 		for($l = 0; $l<$count+2;$l++){
-			
+
 			$path = $path.$tab_ligne[$l].'/';
 		}
 		//echo $path;
-		
+
 		//echo $i;
 		echo ("<br>");
 		echo ("creation du dossier");
@@ -63,7 +63,7 @@ for($i =0 ; $i<count($tab) ; $i++){         //on parcourt notre tableau contenan
 		echo ("<br>");
 		mkdir ($path, 0777, true );		// le true indique que le mkdir est récursif : créer les sous dossier implique création dossiers.
 	}
-	
+
 }
 
 
