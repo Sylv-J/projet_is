028bd004d9b9617ea0f2e8563180d1c5d11ca69f
Sahli Ayoub <ayoub.sahli@depinfonancy.net>
2016-01-27 17:23:53 +0100

    Mise en forme
    Mise du contenu du formulaire d'ajout de concours dans des balises <div>

--- actions/administrateur/formulaire_ajout_table.php
+++ actions/administrateur/formulaire_ajout_table.php
@@ -4,12 +4,16 @@ $db = masterDB::getDB();
 //Pas de Check ici. On demande de rentrer un nom de concours, le check d'existence se fait dans action_ajout_table.php
 ?>
 <!-- HMTL code for the registration form -->
-<form action="../database_request/action_ajout_table.php" method="post">
-<h3>Inscription : </h3>
-<p>
-	Nom concours : <input type="text" name="NomConcours" required><br><br>
+<div class="jumbotron">
+	<div class="container">
+		<form action="../database_request/action_ajout_table.php" method="post">
+			<h3>Inscription : </h3>
+			<p>
+				Nom concours : <input type="text" name="NomConcours" required><br><br>
 
-	</select><br><br>
-	<input type="submit" name='submit' value='register'><br>
-</p>
-</form>
+			</select><br><br>
+			<input type="submit" name='submit' value='register'><br>
+		</p>
+	</form>
+</div>
+</div>
