747eb5fdf8796c5c20ba4efbc827bd45977ae30a
marionbarre <marion.barre@depinfo.net>
2016-01-29 18:29:29 +0100

    suppression de fichiers inutiles

--- messenger/err.php
+++
@@ -1,5 +0,0 @@
-<?php header("refresh:3;URL=../interface_web/index.php") ?>
-
-<center>
-  Vous devez être connecté pour pouvoir utiliser la messagerie interne. <br>Vous allez être redirigé vers la <a href="../interface_web/index.php">page d'acceuil</a> automatiquement.
-</center>
