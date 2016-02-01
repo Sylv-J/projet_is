55f58148dcaf90a5f25327a5b7548fb5a789e701
mmmbao <bao-vi.defaux@depinfonancy.net>
2016-02-01 18:17:07 +0100

    Correction Stats

--- database_request/statsgenerales.php
+++ database_request/statsgenerales.php
@@ -2,7 +2,7 @@
   include_once("../master_db.php");
   $db = masterDB::getDB();
   $req = $db->query("SELECT units_remaining FROM users WHERE user_group = 'correcteur' ");
-  $req2 = $db->query("SELECT id_sons FROM units WHERE id_sons is NULL");
+  $req2 = $db->query("SELECT id_sons FROM units WHERE id_sons is NULL AND id_corrector is NULL");
   $nbcopies=0;
   $nbassignes=0;
   while($donnees = $req->fetch()){
